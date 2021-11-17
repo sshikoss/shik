27eb2f0d373fa000660174a405cfbb31cbf8cab9
shik <sshikoss@gmail.com>
2021-11-17 21:41:17 +0200

    scroll bar

--- js/main.js
+++ js/main.js
@@ -11,8 +11,10 @@ var mybutton = document.getElementById("myBtn");
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
   scrollFunction();
+  scrollbar();
 };
 
+//scroll up
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
@@ -26,7 +28,6 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
 //read more or less button
 function myFunction() {
   var dots = document.getElementById("dots");
@@ -45,3 +46,38 @@ function myFunction() {
 }
 
 console.log(dots.style.display === "none");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+  scrollbar();
+};
+
+//Get scrollup  button
+var mybutton = document.getElementById("myBtn");
+
+//scroll up
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// When the user scrolls the page, execute the bar
+
+function scrollbar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
